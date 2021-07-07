c564ecc2295069f00b44d39489f561f3fa96a387
zhoubofsy <zhoubofsy@hotmail.com>
2021-07-07 15:12:18 +0800

    update cis unit test

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	log "github.com/sirupsen/logrus"
+	"os"
 	"vsphere-go-sdk/cis"
 	"vsphere-go-sdk/common"
 )
@@ -17,19 +18,36 @@ func common_test() {
 	//testBody:="gdhsdsdsghd"
 	//body,_:=json.Marshal(testBody)
 	resBody, _ := client.SendRequest(url, nil, nil, method)
-	log.Println(resBody)
+	log.Debug(resBody)
 
 }
 
 func cis_test() {
-	client := common.NewClient("https://localhost:8088/rest/")
+	code := cis.CodeBase64("root@vsphere.local", "Root@2021")
+	log.Debug("base64:", code)
+
+	client := common.NewClient("https://128.179.0.241/rest/")
+	//client := common.NewClient("http://127.0.0.1/")
+	log.Debug(client)
+
 	c := cis.NewCIS(client)
+	log.Debug(*c)
 
-	sess, _ := c.GetSessionHandle().CreateSession(cis.CodeBase64("username", "password"))
+	shandle := c.GetSessionHandle()
+	log.Debug(shandle)
 
-	log.Println(sess)
+	sess, err := shandle.CreateSession(code)
+	log.Debug("CreateSession: ", sess, err)
+
+	err = shandle.DeleteSession(sess)
+	log.Debug("DeleteSession: ", err)
 }
 
 func main() {
+	log.SetOutput(os.Stdout)     //设置日志的输出为标准输出
+	log.SetLevel(log.DebugLevel) //设置日志的显示级别，这一级别以及更高级别的日志信息将会输出
+	log.SetReportCaller(true)    //设置日志的调用文件，调用函数
+
+	// test cis module
 	cis_test()
 }
