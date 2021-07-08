d208bfd06a9be0f109cfff1e0b03ab80c7f6046d
tiantingting <2565086122@qq.com>
2021-07-08 14:38:48 +0800

    Library修改

--- content/Library.go
+++ content/Library.go
@@ -1,56 +1,34 @@
 package content
 
 import (
-	"fmt"
 	"vsphere-go-sdk/common"
 )
 type Library struct {
-	client common.Client
+	conn Connection
 	uri    string
 }
-func (library *Library) Get() (*Library,*common.Error) {
+type LibraryResult struct {
+
+}
+func (l *Library) Get() ([]LibraryResult,*common.Error) {
+	return nil,common.EOK
+}
+
+func (l *Library) NewItem() *Item {
+	return &Item{
+		conn: l.conn,
+		uri:    l.uri  + "/item",
+	}
+}
+
+type Item struct {
+	conn Connection
+	uri    string
+}
+func (i *Item) GetByLibraryID(libraryId string) (*Library,*common.Error) {
 	return nil,common.EOK
 }
 
 
-type Content struct {
-	library Library
-}
 
-func (o *Content) GetSessionHandle() *Library {
-	return &(o.library)
-}
-
-func NewContent(c common.Client) *Content {
-	return &Content{
-		library: Library{
-			client: c,
-			uri: "com/vmware/cis/session",
-		},
-	}
-}
-
-
-
-func Diaoyong() (*Library, error) {
-
-	pp := &Library{
-		//value: "test",
-	}
-	var lib Operation = pp
-	fmt.Println("Before", pp)
-	methodMap := map[string]interface{}{
-		"Get": lib.Get(),
-	}
-	for k, v := range methodMap {
-		switch k {
-		case "Get":
-			return v.(func(string) (*Library, error))("vcsdsh")
-		case "Create":
-			//v.(func() string)()
-		}
-	}
-	fmt.Println("After", pp)
-	return nil,nil
-}
 
