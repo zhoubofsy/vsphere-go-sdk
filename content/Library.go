1832d7eda586a5715a4bf7222dfac9e90c6782e4
zhoubofsy <zhoubofsy@hotmail.com>
2021-08-03 17:57:15 +0800

    Add get item info of content interface

--- content/Library.go
+++ content/Library.go
@@ -49,12 +49,50 @@ func (i *Item) GetItemByLibraryID(libraryId string) ([]string, error) {
 		log.Error("SendRequestError: ", err)
 		return nil, err
 	}
-	log.WithFields(log.Fields{"ResponseData": string(resp.Data)}).Debug("GetByLibraryID")
 	response := make(map[string][]string)
 	err = json.Unmarshal(resp.Data, &response)
 	if err != nil {
 		return nil, err
 	}
-	log.WithFields(log.Fields{"response: ": response}).Debug("GetByLibraryIDResponse")
 	return response["value"], err
+}
+
+type ValueOfItemInfo struct {
+	Value ItemInfo `json:"value"`
+}
+
+type ItemInfo struct {
+	LastModifiedTime string `json:"last_modified_time"`
+	Cached           bool   `json:"cached"`
+	Type             string `json:"type"`
+	ID               string `json:"id"`
+	Description      string `json:"description"`
+	MetadataVersion  string `json:"metadata_version"`
+	LibraryID        string `json:"library_id"`
+	Version          string `json:"version"`
+	Name             string `json:"name"`
+	SourceID         string `json:"source_id"`
+	CreationTime     string `json:"creation_time"`
+	LastSyncTime     string `json:"last_sync_time"`
+	ContentVersion   string `json:"content_version"`
+	Size             int    `json:"size"`
+}
+
+func (i *Item) GetItemInfoByItemID(id string) (*ItemInfo, error) {
+	header := make(map[string]string)
+	header["vmware-api-session-id"] = i.conn.Sid
+	uri := i.uri + "/id:" + id
+	resp, err := i.conn.Invoker.SendRequest(uri, header, nil, "GET")
+	if err != nil {
+		log.Error("GetItemInfoByItemID Error: ", err)
+		return nil, err
+	}
+
+	vii := ValueOfItemInfo{}
+	err = json.Unmarshal(resp.Data, &vii)
+	if err != nil {
+		log.Error("GetItemInfoByItemID Response Data", string(resp.Data))
+		return nil, err
+	}
+	return &(vii.Value), err
 }
